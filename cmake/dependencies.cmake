8c22a99ac4c85a3595f4b4c4535bf46527c94ee3
头号否 <thfou@qq.com>
2023-05-21 14:58:40 +0800

    提交说明

--- cmake/dependencies.cmake
+++
@@ -1,7 +0,0 @@
- # ==============================================================================
-# Fetching external libraries
-
-if(build_anisetteserver)
-    include(UseDub)
-    DubProject_Add(handy-httpd ~5.2.1)
-endif()
--- lib/provision/package.d
+++
@@ -1,3 +0,0 @@
-module provision;
-
-public import provision.adi;
